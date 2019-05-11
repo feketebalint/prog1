59f1a8e9f86f9112531cd3cd983158dd419932f1
feketebalint <50149997+feketebalint@users.noreply.github.com>
2019-05-11 18:03:33 +0200

    Update PROG_1.py

--- PROG_1.py
+++ PROG_1.py
@@ -5,3 +5,11 @@ def prime(n):
             if n % i == 0:
                 return False
     return True
+
+def lnko(num1,num2):
+    if num1 > num2:
+        num1,num2 = num2,num1
+    for i in range(num1+1,1,-1):
+        if num1 % i == 0 and num2 % i == 0 and prime(i):
+            return i
+ #A kisebb számtóé visszafele megy a for ciklus, hogy hatékonyabb legyen a program.
